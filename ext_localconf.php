dbc8b73dd286e956718efa8a816b90fa7708d400
Christian Wolfram <c.wolfram@chriwo.de>
2025-10-24 08:09:43 +0200

    [TASK] Migrate icon registration

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,9 +9,6 @@ use BeechIt\FalSecuredownload\Hooks\CmsLayout;
 use BeechIt\FalSecuredownload\Hooks\DocHeaderButtonsHook;
 use BeechIt\FalSecuredownload\Hooks\KeSearchFilesHook;
 use BeechIt\FalSecuredownload\Hooks\ProcessDatamapHook;
-use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
-use TYPO3\CMS\Core\Imaging\IconRegistry;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\SignalSlot\Dispatcher;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
@@ -62,16 +59,3 @@ if (ExtensionConfiguration::trackDownloads()) {
         'class' => DownloadStatistics::class,
     ];
 }
-
-/** @var IconRegistry $iconRegistry */
-$iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
-$iconRegistry->registerIcon(
-    'action-folder',
-    SvgIconProvider::class,
-    ['source' => 'EXT:fal_securedownload/Resources/Public/Icons/folder.svg']
-);
-$iconRegistry->registerIcon(
-    'overlay-inherited-permissions',
-    SvgIconProvider::class,
-    ['source' => 'EXT:fal_securedownload/Resources/Public/Icons/overlay-inherited-permissions.svg']
-);
